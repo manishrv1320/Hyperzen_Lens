257289cc519ddea714a6fbde5bf89db9778d0ee2
Manish RV <rvmanish07@gmail.com>
2025-12-02 12:03:37 +0000

    change from"Manaish RV @DSU" to "Manish RV @DSU"

--- src/components/app/footer.tsx
+++ src/components/app/footer.tsx
@@ -2,7 +2,7 @@ export function Footer() {
   return (
     <footer className="py-4 px-4 md:px-6 mt-auto">
       <div className="container mx-auto text-center text-sm text-muted-foreground">
-        <p>Designed and Developed by Manaish RV @DSU</p>
+        <p>Designed and Developed by Manish RV @DSU</p>
       </div>
     </footer>
   );
